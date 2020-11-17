7cae91a6cb9dc725486a130f4c214a122f878bda
Euan Gillespie-Taylor <euan.gillespie@outlook.com>
2020-11-17 08:52:38 +0100

    fixed regex

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_150002) do
+ActiveRecord::Schema.define(version: 2020_11_16_150114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,22 @@ ActiveRecord::Schema.define(version: 2020_11_16_150002) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "windows", force: :cascade do |t|
+    t.string "title"
+    t.integer "floor_space"
+    t.integer "price_per_day"
+    t.text "description"
+    t.text "address"
+    t.integer "longitude"
+    t.integer "latitude"
+    t.boolean "internet"
+    t.boolean "availability_status"
+    t.boolean "heating"
+    t.boolean "lighting"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
