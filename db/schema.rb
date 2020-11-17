3dd5e3aa0e33064315e014fc226fcfe3894a6cc9
Lukas Horstmann <lukas.horstmann.code@gmail.com>
2020-11-17 12:42:26 +0100

    Updated the schema for user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_11_17_114054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "spaces", force: :cascade do |t|
     t.string "title"
@@ -42,10 +41,12 @@ ActiveRecord::Schema.define(version: 0) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "spaces", "users"
-
 end
