06e51faa318917f7864ce5d8110324ab3ed3d105
Lukas Horstmann <lukas.horstmann.code@gmail.com>
2020-11-17 13:30:33 +0100

    :phone_number added to the params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
   end
   private
     def user_params
-      params.require(:user).permit(:first_name, :last_name)
+      params.require(:user).permit(:first_name, :last_name, :phone_number)
     end
 end
