409196620f5c01a37269f3b054b32bfd0972dd1b
Euan Gillespie-Taylor <euan.gillespie@outlook.com>
2020-11-17 09:00:50 +0100

    space model updated

---
+++ app/controllers/spaces_controller.rb
@@ -0,0 +1,43 @@
+class SpacesController < ApplicationController
+  def index 
+        @spaces = Space.all 
+      end
+    
+      def show 
+      end 
+    
+      def new 
+        @space = Space.new 
+      end 
+    
+      def create 
+        @space = Space.new(space_params)
+        if @space.save 
+          redirect_to @space 
+        else 
+          render :new 
+        end
+      end 
+    
+      def edit 
+      end
+    
+      def update 
+        @space.update(space_params)
+        redirect_to space_path(@space)
+      end 
+    
+      def 
+        @space.destroy 
+      end
+    
+      private 
+    
+      def find_space 
+        @space = Space.find(params[:id])
+      end
+    
+      def space_params 
+        #ToDo, identify params to be passed into the space_params
+      end
+end
---
+++ app/models/space.rb
@@ -0,0 +1,9 @@
+class Space < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true
+
+  validates :price_per_day, presence: true
+  validates :address, presence: true, 
+  validates :availability_status, presence: true,
+  # missing validations on floor_space, longitude, latitude, description
+end
